fcc0396cff8dd0a12209396f9508609334a101ee
Maria Viesca <viescamaria3@gmail.com>
2016-11-17 11:02:31 -0600

    Introduccion laravel

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  echo "HOLAA";
+  //  return view('welcome');
+});
+
+Route::get('Hello', function(){
+	echo 'Hola otra vez';
 });
